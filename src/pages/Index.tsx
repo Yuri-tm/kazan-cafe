b5ca4c4a77319c58a7005ce2f46a89a1639e9c45
Yuri-tm <yuritm.tm@gmail.com>
2026-03-24 14:28:06 +0300

    fixes caps in names

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -21,7 +21,7 @@ import R22Img from "@/assets/R22.jpg";
 import templeOfAllReligionsImg from "@/assets/TAR_noBG_small.png";
 import logoImg from "@/assets/Logo.svg";
 import zilantImg from "@/assets/Zilant_noBG_small.png";
-import robotImg from "@/assets/Robot_noBG.png";
+import robotImg from "@/assets/robot_noBG.png";
 import egyptImg from "@/assets/Egypt_noBG_small.png"
 import Footer from "@/components/Footer";
 import { useSiteContent } from "@/hooks/useSiteContent";
